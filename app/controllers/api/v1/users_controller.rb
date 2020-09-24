e93318c85e757c9773cab1c50e390663cbd7cc7f
Matt Readout <matt.readout@gmail.com>
2020-09-24 13:21:12 -0700

    Add action, routes for creating new user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -10,7 +10,24 @@ class Api::V1::UsersController < ApplicationController
         render json: @user
     end
 
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.create(user_params)
+        if @user.valid?
+            render json: { user: UserSerializer.new(@user) }, status: :created
+        else
+            render json: { error: 'failed to create new user' }, status: :not_acceptable
+        end
+    end
+
     private
+    def user_params
+        params.require(:user).permit(:email, :password)
+    end
+    
     def find_user
         @user = User.find(params[:id])
     end
