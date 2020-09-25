b8562408cf139f8d77c0232b719f396ed92d0579
Matt Readout <matt.readout@gmail.com>
2020-09-25 14:17:46 -0700

    Add passwords controller to handle password reset

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     namespace :v1 do
         resources :users, only: [:index, :show, :new, :create]
           post '/login', to: 'auth#create'
+          post '/forgot-password', to: 'passwords#forgot'
+          post '/reset-password', to: 'passwords#reset'
     end
   end
 end
