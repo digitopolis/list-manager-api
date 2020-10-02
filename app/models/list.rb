25cbc43568e5ebf12dd19ce3cfc2addb76691c91
Matt Readout <matt.readout@gmail.com>
2020-10-01 23:25:10 -0700

    Create items lists join table, define associations

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,5 @@
 class List < ApplicationRecord
   belongs_to :user
+  has_many :items_lists
+  has_many :items, through: :items_lists
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_29_224147) do
+ActiveRecord::Schema.define(version: 2020_10_02_043447) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string "title"
+    t.string "creator"
+    t.string "image_url"
+    t.string "medium"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "items_lists", id: false, force: :cascade do |t|
+    t.bigint "item_id", null: false
+    t.bigint "list_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["item_id", "list_id"], name: "index_items_lists_on_item_id_and_list_id"
+    t.index ["list_id", "item_id"], name: "index_items_lists_on_list_id_and_item_id"
+  end
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
@@ -32,5 +50,7 @@ ActiveRecord::Schema.define(version: 2020_09_29_224147) do
     t.string "password_token"
   end
 
+  add_foreign_key "items_lists", "items"
+  add_foreign_key "items_lists", "lists"
   add_foreign_key "lists", "users"
 end
